ebbd8a445df4750c0342a8ae1692d655f458c204
Thibaut Caquet <thcaquet@1A7.42angouleme.fr>
2025-09-06 20:34:37 +0200

    test

---
+++ cub3D.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+
+
+// lib pas sur de les garder la :
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/time.h>
+
+// a voir si ca reste ou pas
+
+#include "parsing.h"
